9b0d54d16e1c01be019d86d06bd6d9c353848d23
Stanislav Sorochan <stas.sorochan@gmail.com>
2023-03-15 01:42:00 +0100

    fix: component import

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import { Provider } from 'react-redux';
 import { store } from './redux/store';
-import { App } from './components/App/App';
+import { App } from '../src/components/App/App';
 
 const container = document.getElementById('root')!;
 const root = createRoot(container);
